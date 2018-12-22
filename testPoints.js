8e6883102778b7b01a304714737cc088329bdac9
trulselg <trulsel@stud.ntnu.no>
2018-12-22 21:49:47 +0100

    Moved bufferFunction over to a separate js file, as well as three test points. Also fixed a small typo in popoup content creation

---
+++ testPoints.js
@@ -0,0 +1,16 @@
+var geoC ={ "type": "FeatureCollection",
+        "features": [
+          { "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-0.13810157775879509, 51.515900258184224]},
+            "properties": {"value": 2, "newProperty": "Non text"}
+          },
+          { "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-0.15046119689941406, 51.49196529933168]},
+            "properties": {"value": 1, "newProperty": "Text!"}
+          },
+            { "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-0.12754440307617188, 51.494690672089675]},
+            "properties": {"value": 2, "newProperty": "Text!"}
+          },
+       ]
+    };
